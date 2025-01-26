f6db9d768f0354f6c0d2692c0038e0c14957e157
Renato Ramos <bacigaluporamos@gmail.com>
2025-01-26 12:26:41 -0300

    P.E_23.py not done yet

--- Project Euler/P.E_23.py
+++ Project Euler/P.E_23.py
@@ -1,42 +1,20 @@
 import time
+from sympy import divisors
 
 
-def divisibles(n):
-    divs =  []
-    i = 1
-
-    while i <= n//2:
-        if n % i == 0:
-            divs.append(i)
-        i += 1
-
-    return divs
-
-def sum_list(n):
-    sum = 0
-
-    for i in n:
-        sum += i
-
-    return sum
-
-
-def main(n):
-    sum = 0
-    i = 1
-
-    while i <= n:
-        if sum_list(divisibles(i)) <= i:
-            sum += i
-        i += 1
-    
-    return sum
-
+abundant_nums = []
 
 start = time.perf_counter()
 
-print(main(28123))
+for num in range(1, 28124):
+    num_divs = divisors(num)[:-1]
+    if sum(num_divs) > num:
+        abundant_nums.append(num)
 
 end = time.perf_counter()
+
+
+print(abundant_nums)
+
 
 print(end - start)
