fb2fda229d069b5898b1e049d14efeca94be00a4
Renato Ramos <bacigaluporamos@gmail.com>
2025-03-22 18:53:56 -0300

    P.E_23.py

--- Project Euler/P.E_23.py
+++ Project Euler/P.E_23.py
@@ -11,10 +11,22 @@ for num in range(1, 28124):
     if sum(num_divs) > num:
         abundant_nums.append(num)
 
+sum2abundants = []
+
+i, j = 0, 0
+
+while i < len(abundant_nums):
+    while j < len(abundant_nums):
+        if abundant_nums[i] + abundant_nums[j] <= 28123:
+            sum2abundants.append(abundant_nums[i] + abundant_nums[j])
+        j += 1
+    i += 1
+    j = i
+
+sum2abundants = list(set(sum2abundants))
+
+print((28123 + 1)*28123//2 - sum(sum2abundants) )
+
 end = time.perf_counter()
-
-
-print(abundant_nums)
-
 
 print(end - start)
